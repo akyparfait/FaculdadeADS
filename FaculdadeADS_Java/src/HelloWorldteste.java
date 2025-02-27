8a59ef25ba2b1de9df7806920f0d4c0862e4e124
Akyparfait <agathariell@gmail.com>
2025-02-27 15:47:05 -0300

    commitando teste para mostrar pro namorado

--- FaculdadeADS_Java/src/HelloWorldteste.java
+++ FaculdadeADS_Java/src/HelloWorldteste.java
@@ -1,6 +1,6 @@
 public class HelloWorldteste {
 
     public static void main(String[] args){
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World mozão!");
     }
 }
